8a12a9b586fc1c6b12630ac52c2ddd137e691053
Pedro Costa <550684+pnmcosta@users.noreply.github.com>
2023-09-16 13:15:56 +0100

    feat: improve filetree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -10,6 +10,42 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		require('neo-tree').setup {}
+		require('neo-tree').setup {
+			open_files_do_not_replace_types = { "terminal", "trouble", "qf" },
+			source_selector = {
+				winbar = true, -- toggle to show selector on winbar
+				statusline = false, -- toggle to show selector on statusline
+			},
+			filesystem = {
+				filtered_items = {
+					visible = false, -- when true, they will just be displayed differently than normal items
+					hide_dotfiles = false,
+					hide_gitignored = false,
+					hide_by_name = {
+						"node_modules",
+						".git",
+					},
+					hide_by_pattern = { -- uses glob style patterns
+						--"*.meta",
+						--"*/src/*/tsconfig.json",
+					},
+					always_show = { -- remains visible even if other settings would normally hide it
+						--".gitignored",
+					},
+					never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+						--".DS_Store",
+						--"thumbs.db"
+					},
+					never_show_by_pattern = { -- uses glob style patterns
+						--".null-ls_*",
+					},
+				},
+				follow_current_file = {
+					enabled = true, -- This will find and focus the file in the active buffer every time
+					--               -- the current file is changed while the tree is open.
+					leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+				},
+			},
+		}
 	end,
 }
