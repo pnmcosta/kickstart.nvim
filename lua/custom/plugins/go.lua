c4b8a8931e50d7b26784e669273a1ec13479c3a6
Pedro Costa <550684+pnmcosta@users.noreply.github.com>
2023-09-23 13:23:11 +0100

    fix: integrate go.nvim into lsp

--- lua/custom/plugins/go.lua
+++ lua/custom/plugins/go.lua
@@ -10,7 +10,15 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 	},
 	config = function()
-		require("go").setup()
+		require("mason").setup()
+		require("mason-lspconfig").setup()
+		require('go').setup {
+			lsp_cfg = false
+			-- other setups...
+		}
+		local cfg = require 'go.lsp'.config() -- config() return the go.nvim gopls setup
+
+		require('lspconfig').gopls.setup(cfg)
 	end,
 	event = { "CmdlineEnter" },
 	ft = { "go", 'gomod' },
