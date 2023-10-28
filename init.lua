0b9bf39f2271fcfb98c9873ed21c03c62049e81f
Pedro Costa <550684+pnmcosta@users.noreply.github.com>
2023-10-28 02:36:47 +0100

    fix: barbar no more

--- init.lua
+++ init.lua
@@ -167,11 +167,18 @@ require('lazy').setup({
     -- See `:help lualine.txt`
     opts = {
       options = {
-        icons_enabled = false,
+        icons_enabled = true,
         theme = 'onedark',
         component_separators = '|',
         section_separators = '',
       },
+      sections = {
+        lualine_a = {
+          {
+            'buffers',
+          }
+        }
+      }
     },
   },
 
